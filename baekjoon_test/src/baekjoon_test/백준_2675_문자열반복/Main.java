cf9ec721f3f54e500d0598545ff7979fc916dbb0
504 <504@DESKTOP-KHUH3DD>
2023-03-24 18:00:44 +0900

    2023-03-24

---
+++ baekjoon_test/src/baekjoon_test/백준_2675_문자열반복/Main.java
@@ -0,0 +1,27 @@
+package baekjoon_test.백준_2675_문자열반복;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader( new InputStreamReader(System.in) );
+
+		StringTokenizer st = new StringTokenizer( br.readLine() );
+		
+		System.out.println( st.nextToken() );
+		int p = Integer.parseInt( st.nextToken() );
+		System.out.println();
+		String[] s = new String [ p ];
+		
+		for( int i=0; i<p; i++ ) {
+			
+			
+		}
+		
+		
+	}
+}
