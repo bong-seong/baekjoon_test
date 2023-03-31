d2d4ee53acf08c074daa92947e202cb957fc46a1
504 <504@DESKTOP-KHUH3DD>
2023-03-31 13:15:17 +0900

    2023-03-31 13:15

---
+++ baekjoon_test/src/baekjoon_test/백준_1316_그룹단어체커/Fail.java
@@ -0,0 +1,36 @@
+package baekjoon_test.백준_1316_그룹단어체커;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Fail {
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader( new InputStreamReader(System.in) );
+		
+		int n = Integer.parseInt( br.readLine() );
+		int count = 0;
+		int check = 0;
+		
+		for( int i=0; i<n; i++ ) {
+			
+			char[] array = br.readLine().toCharArray(); 
+			
+			for( int k=0; k<array.length; k++ ) {
+				count = 0;
+				if( array[k] != array[ k-1 < 0 ? 0 : k-1 ] ) {
+					for( int j=0; j<array.length; j++ ) {
+						if( array[k] == array[j] ) {
+							count++;
+						}
+					}
+				}
+				if( count > 1 ) {
+					break;
+				}
+			}
+			check++;
+		}
+		System.out.println( check );
+	}
+}
--- baekjoon_test/src/baekjoon_test/백준_1316_그룹단어체커/Main.java
+++ baekjoon_test/src/baekjoon_test/백준_1316_그룹단어체커/Main.java
@@ -6,31 +6,42 @@ import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws Exception {
 		
-		BufferedReader br = new BufferedReader( new InputStreamReader(System.in) );
+		
+		BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
 		
 		int n = Integer.parseInt( br.readLine() );
+		
+		int[] array;
+		boolean result;
 		int count = 0;
-		int check = 0;
+	
 		
 		for( int i=0; i<n; i++ ) {
 			
-			char[] array = br.readLine().toCharArray(); 
+			array = new int [ 26 ];
+			String s = br.readLine();
+			int index = s.charAt(0) - 'a';
+			array[index]++;
+			result = true;
 			
-			for( int k=0; k<array.length; k++ ) {
-				count = 0;
-				if( array[k] != array[ k-1 < 0 ? 0 : k-1 ] ) {
-					for( int j=0; j<array.length; j++ ) {
-						if( array[k] == array[j] ) {
-							count++;
-						}
+			for( int j=1; j<s.length(); j++ ) {
+				index = s.charAt(j) - 'a';
+				
+				if( s.charAt(j) != s.charAt(j-1) ) {
+					
+					array[index]++;
+					
+					if( array[index] == 2 ) {
+						result = false;
+						break;
 					}
 				}
-				if( count > 1 ) {
-					break;
-				}
 			}
-			check++;
+			if( result ) {
+				count++;
+			}
 		}
-		System.out.println( check );
+		
+		System.out.println( count );
 	}
 }
