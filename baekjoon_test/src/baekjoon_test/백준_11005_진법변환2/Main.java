86432a4caed626e1028ba62b94e57d948f2df942
김성봉 <ksbthyls@gmail.com>
2023-04-10 20:12:08 +0900

    2023-04-10 20:12

---
+++ baekjoon_test/src/baekjoon_test/백준_11005_진법변환2/Main.java
@@ -0,0 +1,34 @@
+package baekjoon_test.백준_11005_진법변환2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+		
+		StringTokenizer st = new StringTokenizer( br.readLine() , " " );
+		
+		int n = Integer.parseInt( st.nextToken() );
+		int b = Integer.parseInt( st.nextToken() );
+		int sum = 0;
+		
+		ArrayList<Character> list = new ArrayList<>();
+		
+		while( n > 0 ) {
+			if( n%b < 10 ) {
+				list.add( (char)( n%b + '0' ) );
+			}else {
+				list.add( (char)( n%b - 10 + 'A' ) ); 
+			}
+			n /= b;
+		}
+		
+		for( int i=list.size()-1; i>=0; i-- ) {
+			System.out.print( list.get(i) );
+		}
+	}
+}
